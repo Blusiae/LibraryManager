b4da159d5143da44f603770662797d693a089b31
Daniel Kocot <blusiae@gmail.com>
2022-07-21 20:24:43 +0200

    Add book list page functionality

---
+++ LibraryManager/Controllers/BookController.cs
@@ -0,0 +1,28 @@
+﻿using LibraryManager.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManager
+{
+    public class BookController : Controller
+    {
+        private IBookManager _bookManager;
+        private ViewModelMapper _mapper;
+
+        public BookController(IBookManager bookManager, ViewModelMapper mapper)
+        {
+            _bookManager = bookManager;
+            _mapper = mapper;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List(string filterString, bool borrowedOnly = false)
+        {
+            var bookDtos = _bookManager.GetAll(filterString, borrowedOnly);
+            var books = _mapper.Map(bookDtos);
+            return View(books);
+        }
+    }
+}
