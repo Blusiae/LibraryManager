ee0b146d68180947032c27d96dba4389c6f9d2fe
Daniel Kocot <blusiae@gmail.com>
2022-07-22 12:38:01 +0200

    Add borrow list page basic functionalities

---
+++ LibraryManager/Controllers/BorrowController.cs
@@ -0,0 +1,25 @@
+﻿using LibraryManager.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManager
+{
+    public class BorrowController : Controller
+    {
+        private IBorrowManager _borrowManager;
+        private ViewModelMapper _mapper;
+
+        public BorrowController(IBorrowManager borrowManager, ViewModelMapper mapper)
+        {
+            _borrowManager = borrowManager;
+            _mapper = mapper;
+        }
+
+        public IActionResult List()
+        {
+            var borrowDtos = _borrowManager.GetAll();
+            var borrows = _mapper.Map(borrowDtos);
+
+            return View(borrows);
+        }
+    }
+}
