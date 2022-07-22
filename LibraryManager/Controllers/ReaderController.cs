0d993632015fa8f89825057959dce53268e8aa2a
Daniel Kocot <blusiae@gmail.com>
2022-07-22 12:08:44 +0200

    Add reader list page functionality

---
+++ LibraryManager/Controllers/ReaderController.cs
@@ -0,0 +1,29 @@
+﻿using LibraryManager.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManager
+{
+    public class ReaderController : Controller
+    {
+        private IReaderManager _readerManager;
+        private ViewModelMapper _mapper;
+
+        public ReaderController(IReaderManager readerManager, ViewModelMapper mapper)
+        {
+            _readerManager = readerManager;
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List(string filterString)
+        {
+            var readerDtos = _readerManager.GetAll(filterString);
+            var readers = _mapper.Map(readerDtos);
+            return View(readers);
+        }
+    }
+}
