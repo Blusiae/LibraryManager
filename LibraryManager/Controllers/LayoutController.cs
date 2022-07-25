178d134d4f091fe8864d6a5cc6d19c472688669f
Daniel Kocot <blusiae@gmail.com>
2022-07-25 10:50:38 +0200

    Change hard coded text in Book views and Home view to resource files keys

---
+++ LibraryManager/Controllers/LayoutController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace LibraryManager
+{
+    public class LayoutController : Controller
+    {
+        public void SetLanguage(string lang)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo(lang);
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo(lang);
+        }
+    }
+}
